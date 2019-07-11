889201cdbe31d948a7ba349aece7ec6cd9efe0a9
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 21:09:52 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
+  puts "hi"
+  num1 + num2
 
 end
 
 def subtraction(num1, num2)
+  puts "hi"
+  num1 - num2
 
 end
 
 def division(num1, num2)
+  puts "hi"
+  num1 / num2
 
 end
 
 def multiplication(num1, num2)
+  puts "hi"
+  num1 * num2
 
 end
 
 def modulo(num1, num2)
+  puts "hi"
+  num1 % num2
 
 end
 
 def square_root(num)
+  puts (square_root(81)
+  num
 
 end
